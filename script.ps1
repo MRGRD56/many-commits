83917f6f2abb612dd61ae5a45383dde75198be49
MRGRD56 <Bukinist56@mail.ru>
2021-02-12 19:44:59 +0500

    2021-02-12T19:44:59

--- script.ps1
+++ script.ps1
@@ -1,10 +1,9 @@
-$random = [System.Random]::new()
-
 while ($true)
 {
-    [System.IO.File]::WriteAllText("C:\Users\priv-repo\readme.md", $random.NextDouble().ToString())
+    $time = [System.DateTime]::Now.ToString("yyyy-MM-ddTHH\:mm\:ss")
+    [System.IO.File]::WriteAllText("C:\Users\priv-repo\readme.md", $time)
     git add *
-    git commit -m ([System.DateTime]::Now.ToString("yyyy-MM-ddTHH\:mm\:ss"))
+    git commit -m ($time)
     git push origin master
     Start-Sleep 5
 }
