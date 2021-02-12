ccb8bc1c4629a662f9d068a9b139f02d73a07660
MRGRD56 <Bukinist56@mail.ru>
2021-02-12 19:47:23 +0500

    2021-02-12T19:47:23

--- script.ps1
+++ script.ps1
@@ -1,9 +1,8 @@
-while ($true)
+for ($i = 0; $i -lt 50; $i++)
 {
     $time = [System.DateTime]::Now.ToString("yyyy-MM-ddTHH\:mm\:ss")
     [System.IO.File]::WriteAllText("C:\Users\priv-repo\readme.md", $time)
     git add *
     git commit -m ($time)
     git push origin master
-    Start-Sleep 5
 }
