1f68b2e0c0409901452649cdf9c1cb1e6c251e92
MRGRD56 <Bukinist56@mail.ru>
2021-02-16 20:00:56 +0500

    2021-02-16T20:00:56

--- script.ps1
+++ script.ps1
@@ -6,7 +6,7 @@ param
 for ($i = 1; $i -le $count; $i++)
 {
     $time = [System.DateTime]::Now.ToString("yyyy-MM-ddTHH\:mm\:ss")
-    [System.IO.File]::WriteAllText("C:\Users\priv-repo\readme.md", $time)
+    [System.IO.File]::WriteAllText(".\readme.md", $time)
     git add *
     git commit -m ($time)
     git push origin master
