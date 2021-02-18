426b2f18a35506bd0d89fbc948660abb78059bcf
MRGRD56 <kirgol85@gmail.com>
2021-02-18 11:40:00 +0500

    2021-02-18T11:40:00'829

--- script.ps1
+++ script.ps1
@@ -1,18 +1,18 @@
+param 
+(
+    [int]$count = 100
+)
+
 function Push 
 {
     git push origin master
     Write-Host ("SUCCECCFULY PUSHED")
 }
 
-param 
-(
-    [int]$count = 100
-)
-
 for ($i = 1; $i -le $count; $i++)
 {
     $time = [System.DateTime]::Now.ToString("yyyy-MM-ddTHH\:mm\:ss\'fff")
-    [System.IO.File]::WriteAllText(".\readme.md", $time)
+    [System.IO.File]::WriteAllText("C:\Users\SU\many-commits\readme.md", $time)
     git add *
     git commit -m ($time)
     Write-Host ("[Прогресс: " + $i + " / " + $count + "]")
